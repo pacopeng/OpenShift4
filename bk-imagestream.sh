cfe4f71af52d3c3803f5b78d19c7e13f50baeb4d
Paco Peng <pzx_hero_19841002@163.com>
2021-05-14 16:44:09 +0800

    Update bk-imagestream.sh

--- bk-imagestream.sh
+++ bk-imagestream.sh
@@ -1,2 +1,12 @@
-for i in `oc get is -A | grep -v NAME | awk '{print $1":"$2}'`;do  is=${i#*:};ns=${i%:*};echo backup imagestream $is in namespace $ns;oc get is $is -n $ns --export -o yaml > /ocpinstall/backup/namespacebackup/imagestream/is-$is-ns-$ns.yaml;done
+for i in `oc get is -A | grep -v NAME | awk '{print $1":"$2}'`;\
+do  is=${i#*:};ns=${i%:*};echo backup imagestream $is in namespace $ns;\
+oc get is $is -n $ns --export -o yaml > /ocpinstall/backup/namespacebackup/imagestream/is-$is-ns-$ns.yaml;done
 
+
+
+
+for i in `oc get template -A | grep -v DESCRIPTION | awk '{print $1":"$2}'`;\
+do  is=${i#*:};ns=${i%:*};echo backup template $is in namespace $ns;\
+oc get template  $is -n $ns --export -o yaml \
+> /ocpinstall/backup/namespacebackup/template/template-$is-ns-$ns.yaml;\
+done
